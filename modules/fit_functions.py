c5ec96d26ccd5dd02b896ba412fd1896406db075
green-cabbage <hyeonseo_yun@hotmail.com>
2025-06-17 00:41:51 +0200

    adding ratio plot, but I am not too sure about the way I implemented ratio hist

--- modules/fit_functions.py
+++ modules/fit_functions.py
@@ -1,6 +1,7 @@
 import ROOT
 import ROOT as rt
 from typing import Tuple, List, Dict
+ROOT.gStyle.SetOptStat(0) # remove stats box
 
 def getFEWZ_roospline(x, root_path):
     """
@@ -74,7 +75,56 @@ def getShapeModifierHist(x, allCat_hist, subCat_hist, normalize=False, nbins=100
     roo_hist_shapModifier = rt.RooDataHist(rooHist_name, rooHist_name, rt.RooArgSet(x), subCat_th1) 
     return roo_hist_shapModifier
 
-def plot_6_23(x, roo_histData_allCat, subCat_dataHists, SMF_func_l, fitResult, save_fname, normalize=True, nbins=100):
+def getPdfToHist(x, pdf, hist2copy):
+    pdf_hist = hist2copy.Clone("pdf_hist")
+    for i in range(1, pdf_hist.GetNbinsX()+1):
+        xval = pdf_hist.GetXaxis().GetBinCenter(i)
+        x.setVal(xval)
+        # get uncertainty on PDF at this point from fit result
+        pdf_val = pdf.getVal(ROOT.RooArgSet(x))
+        pdf_hist.SetBinContent(i, pdf_val)  # ratio = 1
+        pdf_hist.SetBinError(i, 0) # remove errors
+    return pdf_hist
+
+# def getRatioHist(x, hist, pdf):
+#     ratio_hist = hist.Clone("ratio_hist")
+#     for i in range(1, hist.GetNbinsX()+1):
+#         xval = ratio_hist.GetXaxis().GetBinCenter(i)
+#         x.setVal(xval)
+    
+#         # get uncertainty on PDF at this point from fit result
+#         hist_val = ratio_hist.GetBinContent(i)
+#         pdf_val = pdf.getVal(ROOT.RooArgSet(x))
+#         print(f"bin {i} hist_val: {hist_val}")
+#         print(f"bin {i} pdf_val: {pdf_val}")
+#         ratio_hist.SetBinContent(i, hist_val/pdf_val)  # ratio = 1
+#         ratio_hist.SetBinError(i, 0) # remove errors
+#     return ratio_hist
+
+def getRatioHist(x, hist, pdf):
+    hist_clone = hist.Clone("ratio_hist")
+    hist_pdf = getPdfToHist(x, pdf, hist)
+    # normalize
+    hist_clone.Scale(1/hist_clone.Integral())
+    hist_pdf.Scale(1/hist_pdf.Integral())
+    # for i in range(1, hist_clone.GetNbinsX()+1):
+    #        # get uncertainty on PDF at this point from fit result
+    #     hist_val = hist_clone.GetBinContent(i)
+    #     pdf_val = hist_pdf.GetBinContent(i)
+    #     print(f"bin {i} hist_val: {hist_val}")
+    #     print(f"bin {i} pdf_val: {pdf_val}")
+    ratio_hist = hist_clone
+    ratio_hist.Divide(hist_pdf)
+
+    # Style: black dots with error bars
+    ratio_hist.SetMarkerStyle(20)        # Filled circle
+    ratio_hist.SetMarkerSize(1.0)
+    ratio_hist.SetMarkerColor(ROOT.kBlack)
+    ratio_hist.SetLineColor(ROOT.kBlack)  # Error bars in black
+    return ratio_hist
+    
+def plot_6_23(x, roo_histData_allCat, subCat_dataHists, SMF_pdf_l, fitResult, save_fname, normalize=True, nbins=100):
+    # normalize=False
     x_name = x.GetName()
     for ix in range(len(subCat_dataHists)):
     # for ix in range(1):
@@ -100,9 +150,10 @@ def plot_6_23(x, roo_histData_allCat, subCat_dataHists, SMF_func_l, fitResult, s
         roo_hist_shapModifier = getShapeModifierHist(x, roo_histData_allCat, subCat_dataHists[ix], normalize=normalize, nbins=nbins)
         # plot the SMF fit function first
         roo_hist_shapModifier.plotOn(frame, Invisible=True) # Invisible plot for SMF functions to plot over
-        SMF_func = SMF_func_l[ix]
-        SMF_func.plotOn(frame, VisualizeError=(fitResult, 1), FillColor=rt.kCyan, Components=SMF_func.GetName()) # don't need the specify component name, but I guess it's good practice
-        SMF_func.plotOn(frame, LineColor=rt.kRed)
+        SMF_pdf = SMF_pdf_l[ix]
+        SMF_pdf.plotOn(frame, VisualizeError=(fitResult, 1), FillColor=rt.kCyan, Components=SMF_pdf.GetName()) # don't need the specify component name, but I guess it's good practice
+        pull_hist = frame.pullHist() # to be used later
+        SMF_pdf.plotOn(frame, LineColor=rt.kRed)
         
         # plot the shape modifier data
 
@@ -122,6 +173,32 @@ def plot_6_23(x, roo_histData_allCat, subCat_dataHists, SMF_func_l, fitResult, s
         # Bottom pad start
         pad2.cd()
         ratio_frame= x.frame()
+
+        # SMF_pdf_hist = SMF_pdf.createHistogram(x.GetName()).Clone("SMF_pdf_clone")
+        # ratio_hist = roo_hist_shapModifier.createHistogram(x.GetName()).Clone("ratio_hist")
+        # ratio_hist.Scale(SMF_pdf_hist.Integral() / ratio_hist.Integral())  # normalize
+        # ratio_hist.Divide(SMF_pdf_hist)
+
+        shapeModifier_hist = roo_hist_shapModifier.createHistogram(x.GetName())
+        ratio_hist = getRatioHist(x, shapeModifier_hist, SMF_pdf)
+        ratio_hist.GetYaxis().SetRangeUser(0.9, 1.1)
+        ratio_hist.Draw("E1")
+
+        # ratio_hist = rt.RooDataHist("ratio_hist", "ratio_hist", rt.RooArgSet(x), ratio_hist) 
+        # ratio_hist.plotOn(ratio_frame)
+        
+        # ratio_frame.addPlotable(pull_hist, "P")
+        # ratio_smf_pdf = rt.RooGenericPdf("ratio_smf_pdf", "0.5*@0-0.5*@0 +1", rt.RooArgList(SMF_pdf))
+        # ratio_smf_pdf = rt.RooGenericPdf("ratio_smf_pdf", "@0/@0", rt.RooArgList(SMF_pdf))
+
+        
+        # ratio_smf_pdf.plotOn(ratio_frame, VisualizeError=(fitResult, 1), FillColor=rt.kCyan, Components=SMF_pdf.GetName()) # don't need the specify component name, but I guess it's good practice
+        # # ratio_smf_pdf.plotOn(ratio_frame, VisualizeError=(fitResult, 1), FillColor=rt.kCyan) # don't need the specify component name, but I guess it's good practice
+        # ratio_smf_pdf.plotOn(ratio_frame, LineColor=rt.kRed)
+        
+        # ratio_frame.GetYaxis().SetTitle("Data/Pred")
+        # ratio_frame.GetXaxis().SetTitle("m_{\mu\mu} [GeV]")
+        # ratio_frame.Draw()
         
         canvas.Update()
         canvas.Draw()
