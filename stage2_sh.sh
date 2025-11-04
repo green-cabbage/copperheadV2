e871fa9c84af777f2a732c73a8c2e539904738f2
green-cabbage <hyeonseo_yun@hotmail.com>
2025-11-04 02:49:38 +0100

    Add rpt as input var BDT onto copperheadV2 workflow (commit: https://github.com/green-cabbage/Run2_MVA_trainer/commit/1e8312d2042accce69ffb8fcf993eaf1092b6006) calculate new target yields and optimize BDT edges for V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_setRandomSeed_addOnlyRpt . Adrresses ram1123#67.

--- stage2_sh.sh
+++ stage2_sh.sh
@@ -44,7 +44,8 @@ category="ggh"
 # model="V2_fullRun_Jun21_2025_1n2Revised_ReProd_RemoveRptKeepYr"
 # model="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_setRandomSeed_FillNoneCorrected"
 # model="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_setRandomSeed_annhilateWeight"
-model="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_setRandomSeed_applyClassWgtBalance"
+# model="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_setRandomSeed_applyClassWgtBalance"
+model="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_setRandomSeed_addOnlyRpt"
 
 
 # stage2_save_path="/depot/cms/users/yun79/hmm/copperheadV1clean/$label/${model}_${category}/stage2_output" 
