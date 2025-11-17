b98c2ca8fca5c4e754e3cd1c0046a85d3d782c5c
green-cabbage <hyeonseo_yun@hotmail.com>
2025-11-17 13:33:04 +0100

    Remove ebe mass res for bkg samples BDT onto copperheadV2 workflow (commit: https://github.com/green-cabbage/Run2_MVA_trainer/commit/07313742cec08b3f95287a747b34c8b456f9ea1c) calculate new target yields and optimize BDT edges for V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_ebeSigOnly . Adrresses ram1123#67.

--- stage2_sh.sh
+++ stage2_sh.sh
@@ -52,7 +52,8 @@ category="ggh"
 # model="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_setRandomSeed_annhilateWeight_separateYears_Nov11repeat"
 # model="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_setRandomSeed_annhilateWeight_addOnlyYear"
 # model="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_applyClassWgtBalance_addOnlyYear_Nov14repeat"
-model="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_applyClassWgtBalance_addOnlyYear_noEbeMass"
+# model="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_applyClassWgtBalance_addOnlyYear_noEbeMass"
+model="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_ebeSigOnly"
 
 
 # stage2_save_path="/depot/cms/users/yun79/hmm/copperheadV1clean/$label/${model}_${category}/stage2_output" 
