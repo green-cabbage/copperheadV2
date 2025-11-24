062911e149d32905caf85baf71ba5f2486424ea3
green-cabbage <hyeonseo_yun@hotmail.com>
2025-11-24 18:03:47 +0100

    Add validation plots to V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_StdClsWgt_addEbeMassRel using setup from https://github.com/green-cabbage/copperheadV2/commit/0e317cad5b12c583b1dac20f2e4e047d0b9c0a07. Adrresses ram1123#67.

--- validation/ggH/categorization/run_script.sh
+++ validation/ggH/categorization/run_script.sh
@@ -41,7 +41,8 @@ stage2_load_path="/depot/cms/users/yun79/hmm/copperheadV1clean/$label/stage1_out
 # model_name="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_ebeSigOnly"
 # model_name="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_StdClsWgt"
 # model_name="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_StdClsWgt_Variation1"
-model_name="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_StdClsWgt_Variation2"
+# model_name="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_StdClsWgt_Variation2"
+model_name="V2_fullRun_Jun21_2025_1n2Revised_ReProduction_Run3_StdClsWgt_addEbeMassRel"
 
 
 # # category="${model_name}_ggh"
